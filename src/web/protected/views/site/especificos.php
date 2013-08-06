7112a6b1414139aa2cd1c7179a955e1636ff41f2
NEWLIFEVE <mark182182@gmail.com>
2013-08-06 17:05:12 -0430

    archivos faltantes para interfaz por capas

---
+++ src/web/protected/views/site/especificos.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+                    <div  id="atras"><ul class="thumbnails">
+                            <li class="span1">
+                                <div id="icono"  href="index" class="">
+                                    <rigth>  <h1> <a id="flecha-backward" href="index" class="tooltip-test"><</a></h1></rigth>
+                                </div>
+                            </li>
+                        </ul> 
+                    </div>
+<div class="Rotate-90">ESPECIFICOS</div>
+
+                    <div id="instruccion2" class="span1">
+                      EN DESARROLLO!!
+                    </div> 
---
+++ src/web/protected/views/site/rutinarios.php
@@ -0,0 +1,140 @@
+<?php 
+
+$this->layout=$this->getLayoutFile('menuContent');
+
+?>
+<!--/**********************************************INICIO CAPA RUTINARIOS*****************************************************************************************/-->                   
+<html>
+    <head>
+    </head>
+    <body>
+                <div class="capaOculta">
+                    <div  id="atras"><ul class="thumbnails">
+                            <li class="span1">
+                                <div id="icono" >
+                                   <h1> <a id="flecha-backward" href="/" class="tooltip-test"><</a></h1>
+                                </div>
+                            </li>
+                        </ul> 
+                    </div>
+                   <div class="Rotate-90 hidden-phone hidden-tablet">RUTINARIOS</div>
+                    <div id="barraVerde1" class="span2 hidden-phone" >
+                    </div> 
+                    <div id="instruccion" class="span1">
+                        Seleccione una fecha
+                    </div>  
+
+                    <div id="datepicker" class="span6">
+                    </div>
+                    <div id="mail"  class="span1">
+                        <a href="#" rel="tooltip" 
+                           title="Enviar Reportes a su Correo Electronico" 
+                           class="tooltip-test"><img src="/images/mail.png"  width="95px" height="95px"
+                                                  value="Activar Función"> 
+                        </a>
+                    </div>
+                    <div id="excel"  class="span1">
+                        <a href="#" rel="tooltip" 
+                           title="Exportar Reportes en Excel" 
+                           class="tooltip-test"><img src="/images/excel.png"  width="75px" height="75px"
+                                                  value="Activar Función">
+                        </a>
+                    </div>
+                    <div id="barraVerde2" class="span2" >
+                    </div>
+                    <div id="instruccion2" class="span1">
+                        Seleccione los Reportes
+                    </div>  
+                    <div id="tablagris" class="span2" >
+
+                        <table>
+                            <tr>
+                                <td width="47">
+                                    <h3>
+                                        <label class="checkbox">
+<!--                                            <input type="checkbox" class="custom-checkbox" onclick="marcar(this);" />-->
+                                            <input type="checkbox" value="todos" id="todos" class="custom-checkbox" name="todos" onClick="marcar(this);">
+                                            <label for="todos"><h4></h4></label>
+                                        </label>
+                                    </h3>
+                                </td>
+                                <td width="209" id="tdtodos">
+                                    <label for="todos"><h4>Todos</h4></label>
+                                </td>
+                            </tr>
+                            <tr>  
+                                <td>
+                                    <label class="checkbox">
+                                        <input type="checkbox" value="AIR" id="AIR" class="custom-checkbox" name="AIR"> 
+                                        <label for="AIR"><h4></h4></label>
+                                    </label>
+                                </td>
+                                <td id="td1">
+                                    <label for="AIR"><h4>Alto Impacto Retail(+1$)</h4></label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label class="checkbox">
+                                        <input type="checkbox" value="AI10" id="AI10" class="custom-checkbox" name="AI10"> 
+                                        <label for="AI10"><h4></h4></label>
+                                    </label>
+                                </td>
+                                <td id="td2">
+                                   <label for="AI10"><h4>Alto Impacto(+10$)</h4></label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label class="checkbox">
+                                        <input type="checkbox" value="PN" id="PN" class="custom-checkbox" name="PN"> 
+                                        <label for="PN"><h4></h4></label>
+                                    </label>
+                                </td>
+                                <td id="td3">
+                                    <label for="PN"><h4>Posicion Neta</h4></label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label class="checkbox">
+                                        <input type="checkbox" value="otros" id="otros" class="custom-checkbox" name="otros"> 
+                                        <label for="otros"><h4></h4></label>
+                                    </label>
+                                </td>
+                                <td id="td4">
+                                    <label for="otros"><h4>Otros</h4></label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label class="checkbox">
+                                        <input type="checkbox" value="otros2" id="otros2" class="custom-checkbox" name="otros2"> 
+                                        <label for="otros2"><h4></h4></label>
+                                    </label>
+                                </td>
+                                <td id="td5">
+                                    <label for="otros2"><h4>Otros</h4></label>
+                                </td>
+                            </tr>
+                            <tr> 
+                                <td>
+                                    
+                                </td> 
+                                <td>
+                                    <input type="hidden"  id="datepicker_value"/>                                        
+                                </td></tr>
+                        </table>
+                    </div> 
+                </div>
+        <div id="respuesta"></div> 
+        <script src="/js/jquery-ui.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/views.js"/></script>
+
+</body>
+</html>
+ <!--/**********************************************FIN CAPA RUTINARIOS*****************************************************************************************/-->                   
+
+        <!--<script src="/js/jquery-1.9.1.js"></script>-->
+        
+<!--        <script type="text/javascript" src="jquery-1.3.2.min.js"></script>-->
