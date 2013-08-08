6e7d64b9d9a3f13dbd687d96705814c3929d9418
Manuel Zambrano <mmzmm3z@gmail.com>
2013-08-08 13:50:21 -0430

    terminada opcion para alto impacto +10$

--- src/web/protected/controllers/SiteController.php
+++ src/web/protected/controllers/SiteController.php
@@ -1,46 +1,55 @@
 <?php
-
-class SiteController extends Controller {
-
+/**
+* 
+*/
+class SiteController extends Controller
+{
     /**
-     * Declares class-based actions.
-     */
-    public function actions() {
+    * Declares class-based actions.
+    */
+    public function actions()
+    {
         return array(
             // captcha action renders the CAPTCHA image displayed on the contact page
-            'captcha' => array(
-                'class' => 'CCaptchaAction',
-                'backColor' => 0xFFFFFF,
-            ),
+            'captcha'=>array(
+                'class'=>'CCaptchaAction',
+                'backColor'=>0xFFFFFF,
+                ),
             // page action renders "static" pages stored under 'protected/views/site/pages'
             // They can be accessed via: index.php?r=site/page&view=FileName
-            'page' => array(
-                'class' => 'CViewAction',
-            ),
-        );
+            'page'=>array(
+                'class'=>'CViewAction',
+                ),
+            );
     }
 
     /**
      * This is the default 'index' action that is invoked
      * when an action is not explicitly requested by users.
      */
-    public function actionIndex() {
+    public function actionIndex()
+    {
         // renders the view file 'protected/views/site/index.php'
         // using the default layout 'protected/views/layouts/main.php'
-        if (!Yii::app()->user->isGuest) {
+        if(!Yii::app()->user->isGuest)
+        {
             $this->render('index');
-        } else {
+        }
+        else
+        {
             $model = new LoginForm;
             // if it is ajax validation request
-            if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') {
+            if(isset($_POST['ajax']) && $_POST['ajax'] === 'login-form')
+            {
                 echo CActiveForm::validate($model);
                 Yii::app()->end();
             }
             // collect user input data
-            if (isset($_POST['LoginForm'])) {
+            if(isset($_POST['LoginForm']))
+            {
                 $model->attributes = $_POST['LoginForm'];
                 // validate user input and redirect to the previous page if valid
-                if ($model->validate() && $model->login())
+                if($model->validate() && $model->login())
                     $this->redirect(Yii::app()->user->returnUrl);
             }
             // display the login form
@@ -51,9 +60,11 @@ class SiteController extends Controller {
     /**
      * This is the action to handle external exceptions.
      */
-    public function actionError() {
-        if ($error = Yii::app()->errorHandler->error) {
-            if (Yii::app()->request->isAjaxRequest)
+    public function actionError()
+    {
+        if($error = Yii::app()->errorHandler->error)
+        {
+            if(Yii::app()->request->isAjaxRequest)
                 echo $error['message'];
             else
                 $this->render('error', $error);
@@ -61,58 +72,72 @@ class SiteController extends Controller {
     }
 
     /**
-     * Displays the contact page
-     */
-    public function actionRutinarios() {
+    * renderiza vista rutinarios
+    */
+    public function actionRutinarios()
+    {
         $this->render('rutinarios');
     }
 
-    public function actionPersonalizados() {
+    /**
+    * Renderiza vista personalizados
+    */
+    public function actionPersonalizados()
+    {
         $this->render('personalizados');
     }
 
-    public function actionEspecificos() {
+    /**
+    * renderiza vista especificos
+    */
+    public function actionEspecificos()
+    {
         $this->render('especificos');
     }
 
-    public function actionContact() {
-        $model = new ContactForm;
-        if (isset($_POST['ContactForm'])) {
-            $model->attributes = $_POST['ContactForm'];
-            if ($model->validate()) {
-                $name = '=?UTF-8?B?' . base64_encode($model->name) . '?=';
-                $subject = '=?UTF-8?B?' . base64_encode($model->subject) . '?=';
-                $headers = "From: $name <{$model->email}>\r\n" .
+    /**
+    *
+    */
+    public function actionContact()
+    {
+        $model=new ContactForm;
+        if(isset($_POST['ContactForm']))
+        {
+            $model->attributes=$_POST['ContactForm'];
+            if($model->validate())
+            {
+                $name='=?UTF-8?B?'.base64_encode($model->name).'?=';
+                $subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
+                $headers="From: $name <{$model->email}>\r\n".
                         "Reply-To: {$model->email}\r\n" .
                         "MIME-Version: 1.0\r\n" .
                         "Content-type: text/plain; charset=UTF-8";
-
                 mail(Yii::app()->params['adminEmail'], $subject, $model->body, $headers);
                 Yii::app()->user->setFlash('contact', 'Thank you for contacting us. We will respond to you as soon as possible.');
                 $this->refresh();
             }
         }
-
         $this->render('contact', array('model' => $model));
     }
 
     /**
      * Displays the login page
      */
-    public function actionLogin() {
+    public function actionLogin()
+    {
         $model = new LoginForm;
-
         // if it is ajax validation request
-        if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') {
+        if(isset($_POST['ajax']) && $_POST['ajax'] === 'login-form')
+        {
             echo CActiveForm::validate($model);
             Yii::app()->end();
         }
-
         // collect user input data
-        if (isset($_POST['LoginForm'])) {
-            $model->attributes = $_POST['LoginForm'];
+        if(isset($_POST['LoginForm']))
+        {
+            $model->attributes=$_POST['LoginForm'];
             // validate user input and redirect to the previous page if valid
-            if ($model->validate() && $model->login())
+            if($model->validate() && $model->login())
                 $this->redirect(Yii::app()->user->returnUrl);
         }
         // display the login form
@@ -122,26 +147,42 @@ class SiteController extends Controller {
     /**
      * Logs out the current user and redirect to homepage.
      */
-    public function actionLogout() {
+    public function actionLogout()
+    {
         Yii::app()->user->logout();
         $this->redirect(Yii::app()->homeUrl);
     }
 
-    public function actionEnviarMail() {
-        $fecha = '07/22/2013';
+    /**
+    * Action encargada de envuiar por mail el tipo de reporte seleccionado,
+    * las especificaciones seran recibidas desde el array $_POST
+    */
+    public function actionEnviarmail()
+    {
+        $fecha=null;
+        $correos=null;
         $user = 'angelo08121987@gmail.com';
-        $asunto = 'TEST ACTION';
-//            $fecha = $_POST['fecha'];
-//            if (isset($_POST['AI10'])){
-//                $correos = Yii::app()->reportes->AltoIMpacto($fecha);
-//            }
-        $correos = Yii::app()->reportes->AltoIMpacto($fecha);
+        if(isset($_POST['fecha']))
+            $fecha=$_POST['fecha'];
+        $asunto = 'Correo de prueba de la fecha: '.$fecha;
+
+        if(isset($_POST['AIR']))
+            $correos.="Alto Impacto Retail (+1$)<br>";
+        if(isset($_POST['AI10']))
+            $asunto="Alto Impacto (+10$)";
+            $correos = Yii::app()->reportes->AltoIMpacto($fecha);
+        if(isset($_POST['PN']))
+            $correos.="Posicion Neta<br>";
+        if(isset($_POST['otros']))
+            $correos.="Otros<br>";
+        if(isset($_POST['otros2']))
+            $correos.="Otros 2<br>";
         Yii::app()->mail->enviar($correos, $user, $asunto);
-//        $this->render('personalizados');
+        echo "Su correo fue enviado exitosamente :)";
     }
 
-    public function actionExportExcel() {
+    public function actionExportExcel()
+    {
         
     }
-
 }
