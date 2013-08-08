b34118f7457ef28a307262825351dc209be7595f
Manuel Zambrano <mmzmm3z@gmail.com>
2013-08-08 13:44:36 -0430

    terminada funcion de ajax

--- src/web/js/views.js
+++ src/web/js/views.js
@@ -1,3 +1,6 @@
+/**
+*
+*/
 var selector=function(id)
 {
     this.variable=id;
@@ -7,13 +10,16 @@ selector.prototype.run=function()
     this.objeto=$(this.variable);
     this.objeto.datepicker(
     {
+        dateFormat: 'yy-mm-dd',
         onSelect: function(dateText, inst)
         {
             $("#datepicker_value").val(dateText);
         }
     });
 }
-
+/**
+*
+*/
 var navegar=function()
 {
     this.enlaces='a#flecha-forward, a#flecha-backward';
@@ -46,64 +52,6 @@ navegar.prototype.pisaAqui=function()
     });
 }
 
-            $(function() 
-            {
-                var formulario = $("#formRutinarios").serialize();
-                alert("Selecciono AltoImpacto +10$ para la Fecha:" + fecha);
-                $.ajax({
-                    url: $(this).attr("/site/SiteController/EnviarMail"),
-                    data: "fecha=" + fecha,
-                    type: $(this).attr("post"),
-                    $("#results").text(formulario);
-                     })
-            }
-            );
-
-   
-   
-   
-    $(function() {
-                $("#mail").click(function() {
-                var fecha = $("#datepicker_value").val();
-                alert(fecha);
-                if(fecha!=""){
-                if($("#AI10").is(":checked")){                    
-                    alert("Selecciono AltoImpacto +10$ para la Fecha:"+fecha);
-                    $.ajax({
-
-                       url: $(this).attr("/site/SiteController/EnviarMail"),
-
-                        //url: 'PHPMailer_5.2.4/AltoImpacto.php',
-                     //   url: 'site/Enviarmail',
-
-                        data: "fecha="+fecha,
-                       type: $(this).attr("post"),
-                       data: $(this).serialize(), 
-                        success: function(data){
-                            alert (data);
-//                           $('#respuestaAI').html(data);
-                        }
-                    });
-                }
-                if($("#AIR").is(":checked")){                    
-                    alert("Selecciono AltoImpactoRetail +1$ para la Fecha:"+fecha);
-//                    $.ajax({
-//                        url: 'PHPMailer_5.2.4/AltoImpactoRetail.php',
-//                        data: "fecha="+fecha,
-//                        type: 'get',
-//                        success: function(data){
-//                            //alert (data);
-//                            $('#respuestaAIR').html(data);
-//                        }
-//                    });
-                }
-                }else{
-                alert('seleccione una fecha');
-                }
-        });
-        
-            });     
-
 navegar.prototype.ida=function()
 {
     var self=this;
@@ -122,6 +70,53 @@ navegar.prototype.vuelta=function()
         self.objetoMain.toggle('slide');
     });
 }
+/**
+*
+*/
+var ajax=function()
+{
+    this.formulario=null;
+    this.mail="/site/enviarmail";
+}
+ajax.prototype.run=function()
+{
+    var self=this;
+    $('#mail,#excel').on('click',function(e)
+    {
+        e.preventDefault();
+        var tipo=$(this).attr('id');
+        if(tipo=="mail")
+        {
+            self.getForm();
+            self.enviarMail();
+        }
+        else
+        {
+            alert("Excel aun en desarrollo");
+        }
+    });
+}
+ajax.prototype.getForm=function()
+{
+    this.formulario=$("#formRutinarios").serializeArray();
+}
+ajax.prototype.enviarMail=function()
+{
+    var self=this;
+    var opciones=
+    {
+        url:this.mail,
+        data:this.formulario,
+        type:'POST'
+    };
+    $.ajax(opciones).done(function(datos)
+    {
+        alert(datos);
+    }).fail(function()
+    {
+        alert("Error");
+    });
+}
 
 function marcar(source)
 {
@@ -137,8 +132,10 @@ function marcar(source)
 
 var ventana=new navegar();
 var fecha=new selector("#datepicker");
+var ejecutar=new ajax();
 $(document).on('ready',function()
 {
+    ejecutar.run();
     ventana.run();
     fecha.run();
     marcar();
