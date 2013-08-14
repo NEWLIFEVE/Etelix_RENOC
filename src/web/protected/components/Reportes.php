6bc237caa3db3fdb0fe1692572b3d393f8563814
Manuel Zambrano <mmzmm3z@gmail.com>
2013-08-14 14:48:34 -0430

    modificando consultas de clientes

--- src/web/protected/components/Reportes.php
+++ src/web/protected/components/Reportes.php
@@ -22,15 +22,14 @@ class reportes extends CApplicationComponent
         //$conection = pg_connect("host=192.168.1.239 port=5432 dbname=sori user=postgres password=123");
         /********************************* SENTENCIAS SQL - COMIENZO *********************************/
         //Selecciono los totales por clientes
-        $sqlClientes="SELECT c.name AS Cliente, x.TOTALCALLS AS TotalCalls, x.CALLS AS CompleteCalls, x.MINUTOS AS Minutos,x.PDD AS Pdd, x.COST AS Cost, x.REVENUE AS Revenue, x.MARGEN AS Margin
-                        FROM(
-                            SELECT b.id_carrier_customer AS CLIENTE, SUM(b.complete_calls) AS CALLS, SUM(b.complete_calls+b.incomplete_calls) AS TOTALCALLS, SUM(b.minutes) AS MINUTOS, SUM(b.pdd_calls) AS PDD, SUM(b.cost) AS COST, SUM(b.revenue) AS REVENUE, CASE  WHEN SUM(b.margin)>10 THEN SUM(b.margin) ELSE 0 END AS MARGEN
-                                FROM balance b, carrier c, destination_int d
-                                WHERE b.date_balance = '$fecha' AND b.id_destination_int IS NOT NULL AND b.id_carrier_supplier = c.id AND c.name NOT LIKE 'Unknow%' AND b.id_destination_int=d.id AND d.name NOT LIKE 'Unknow%'
-                                GROUP BY b.id_carrier_customer
-                                ORDER BY MARGEN DESC) x, carrier c
-                        WHERE x.MARGEN > 10 AND x.CLIENTE = c.id
-                        ORDER BY x.MARGEN DESC;";
+        $sqlClientes="SELECT c.name AS cliente, x.total_calls, x.complete_calls, x.minutes, x.asr, x.acd, x.pdd, x.cost, x.revenue, x.margin, (((x.revenue*100)/x.cost)-100) AS margin_percentage
+                      FROM(SELECT id_carrier_customer, SUM(incomplete_calls+complete_calls) AS total_calls, SUM(complete_calls) AS complete_calls, SUM(minutes) AS minutes, (SUM(complete_calls)*100/SUM(incomplete_calls+complete_calls)) AS asr, (SUM(minutes)/SUM(incomplete_calls+complete_calls)) AS acd, SUM(pdd_calls) AS pdd, SUM(cost) AS cost, SUM(revenue) AS revenue, SUM(margin) AS margin
+                           FROM balance
+                           WHERE date_balance='$fecha' AND id_carrier_supplier<>(SELECT id FROM carrier WHERE name='Unknown_Carrier') AND id_destination_int<>(SELECT id FROM destination_int WHERE name='Unknown_Destination')
+                           GROUP BY id_carrier_customer
+                           ORDER BY margin DESC) x, carrier c
+                      WHERE x.margin > 10 AND x.id_carrier_customer = c.id
+                      ORDER BY x.margin DESC";
         //Selecciono la suma de todos los totales mayores a 10 dolares de margen
         $sqlClientesTotal="SELECT 'TOTAL' AS etiqueta, SUM(x.TOTALCALLS) AS TotalCalls, SUM(x.CALLS) AS CompleteCalls, SUM(x.MINUTOS) AS Minutos, SUM(x.PDD) AS Pdd, SUM(x.COST) AS Cost, SUM(x.REVENUE) AS Revenue, SUM(x.MARGEN) AS Margin
                             FROM(
@@ -1741,10 +1740,10 @@ class reportes extends CApplicationComponent
                 $color="<tr style='background-color:#FFC8AE; color:#584E4E;'>";
                 break;
             case 2:
-                $color="<tr style='background-color:#AFD699; color:#584E4E;'>";
+                $color="<tr style='background-color:#B3A5CF; color:#584E4E;'>";
                 break;
             case 3:
-                $color="<tr style='background-color:#B3A5CF; color:#584E4E;'>";
+                $color="<tr style='background-color:#AFD699; color:#584E4E;'>";
                 break;
             case 4:
                 $color="<tr style='background-color:#F8B6C9; color:#584E4E;'>";
