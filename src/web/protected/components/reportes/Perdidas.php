0005f27f0fa7af36710364a764f3581931ef5a90
Manuel Zambrano <mmzmm3z@gmail.com>
2013-08-23 17:29:25 -0430

    modificacion de reporte perdidas para incluir campos de costo y revenue

--- src/web/protected/components/reportes/Perdidas.php
+++ src/web/protected/components/reportes/Perdidas.php
@@ -6,21 +6,21 @@ class Perdidas extends Reportes
         $cuerpo="<div>
                   <table style='font:13px/150% Arial,Helvetica,sans-serif;'>
                   <thead>";
-        $cuerpo.=self::cabecera(array('Ranking','Cliente','Destino','Proveedor','Minutos','Perdidas'),'background-color:#615E5E; color:#62C25E; width:10%; height:100%;');
+        $cuerpo.=self::cabecera(array('Ranking','Cliente','Destino','Proveedor','Margen','Minutos','Costo', 'Revenue'),'background-color:#615E5E; color:#62C25E; width:10%; height:100%;');
         $cuerpo.="</thead>
                  <tbody>";
         //Selecciono los totales por clientes
-        $sql="SELECT c.name AS cliente, d.name AS destino, s.name AS proveedor, b.minutes AS minutes, b.margin AS margin
-                      FROM(SELECT id_carrier_customer, id_destination_int, id_carrier_supplier, SUM(minutes) AS minutes, SUM(margin) AS margin
-                        FROM balance
-                        WHERE date_balance='$fecha' AND id_carrier_supplier<>(SELECT id FROM carrier WHERE name='Unknown_Carrier') AND id_destination_int<>(SELECT id FROM destination_int WHERE name='Unknown_Destination')
-                        GROUP BY id_carrier_customer, id_destination_int, id_carrier_supplier
-                        ORDER BY margin ASC) b,
-                           carrier c,
-                           carrier s,
-                           destination_int d
-                      WHERE b.id_carrier_customer=c.id AND d.id=b.id_destination_int AND b.id_carrier_supplier=s.id AND b.margin<0
-                      ORDER BY margin ASC";
+        $sql="SELECT c.name AS cliente, d.name AS destino, s.name AS proveedor, b.margin AS margin, b.minutes AS minutes, b.cost AS cost, b.revenue AS revenue
+              FROM(SELECT id_carrier_customer, id_destination_int, id_carrier_supplier, SUM(margin) AS margin, SUM(minutes) AS minutes, SUM(cost) AS cost, SUM(revenue) AS revenue
+                FROM balance
+                WHERE date_balance='$fecha' AND id_carrier_supplier<>(SELECT id FROM carrier WHERE name='Unknown_Carrier') AND id_destination_int<>(SELECT id FROM destination_int WHERE name='Unknown_Destination')
+                GROUP BY id_carrier_customer, id_destination_int, id_carrier_supplier
+                ORDER BY margin ASC) b,
+                   carrier c,
+                   carrier s,
+                   destination_int d
+              WHERE b.id_carrier_customer=c.id AND d.id=b.id_destination_int AND b.id_carrier_supplier=s.id AND b.margin<0
+              ORDER BY margin ASC";
         $balances=Balance::model()->findAllBySql($sql);
         if($balances!=null)
         {
@@ -41,10 +41,16 @@ class Perdidas extends Reportes
                             $balance->proveedor.
                          "</td>
                           <td style='text-align: left;".self::colorEstilo($pos)."' class='totalCalls'>".
-                            Yii::app()->format->format_decimal($balance->minutes).
+                            Yii::app()->format->format_decimal($balance->margin,5).
                          "</td>
                           <td style='text-align: left;".self::colorEstilo($pos)."' class='completeCalls'>".
-                            Yii::app()->format->format_decimal($balance->margin,5).
+                            Yii::app()->format->format_decimal($balance->minutes).
+                         "</td>
+                         <td style='text-align: left;".self::colorEstilo($pos)."' class='completeCalls'>".
+                            Yii::app()->format->format_decimal($balance->cost).
+                         "</td>
+                         <td style='text-align: left;".self::colorEstilo($pos)."' class='completeCalls'>".
+                            Yii::app()->format->format_decimal($balance->revenue).
                          "</td>
                         </tr>";
             }
@@ -55,13 +61,13 @@ class Perdidas extends Reportes
                         <td colspan='5'>No se encontraron resultados</td>
                      </tr>";
         }
-        $cuerpo.=self::cabecera(array('Ranking','Cliente','Destino','Proveedor','Minutos','Perdidas'),'background-color:#615E5E; color:#62C25E; width:10%; height:100%;');
+        $cuerpo.=self::cabecera(array('Ranking','Cliente','Destino','Proveedor','Margen','Minutos','Costo','Revenue'),'background-color:#615E5E; color:#62C25E; width:10%; height:100%;');
         //Selecciono la suma de todos los totales mayores a 10 dolares de margen
-        $sqlTotal="SELECT SUM(b.margin) AS margin
-                   FROM(SELECT SUM(margin) AS margin
-                        FROM balance
-                        WHERE date_balance='$fecha' AND id_carrier_supplier<>(SELECT id FROM carrier WHERE name='Unknown_Carrier') AND id_destination_int<>(SELECT id FROM destination_int WHERE name='Unknown_Destination')
-                        GROUP BY id_carrier_customer, id_destination_int, id_carrier_supplier
+        $sqlTotal="SELECT SUM(b.margin) AS margin, SUM(b.cost) AS cost, SUM(b.revenue) AS revenue
+                   FROM(SELECT SUM(margin) AS margin, SUM(cost) AS cost, SUM(revenue) AS revenue
+                         FROM balance
+                         WHERE date_balance='$fecha' AND id_carrier_supplier<>(SELECT id FROM carrier WHERE name='Unknown_Carrier') AND id_destination_int<>(SELECT id FROM destination_int WHERE name='Unknown_Destination')
+                         GROUP BY id_carrier_customer, id_destination_int, id_carrier_supplier
                         ORDER BY margin ASC) b
                    WHERE b.margin<0";     
         
@@ -76,12 +82,18 @@ class Perdidas extends Reportes
                         <td style='background-color:#999999; color:#FFFFFF;'>
                         </td>
                         <td style='background-color:#999999; color:#FFFFFF;'>
-                        </td>
-                        <td style='background-color:#999999; color:#FFFFFF;'>
                         TOTAL
-                        </td>
+                        </td>                        
                         <td style='background-color:#999999; color:#FFFFFF;'>".
                             Yii::app()->format->format_decimal($Total->margin,5).
+                       "</td>
+                        <td style='background-color:#999999; color:#FFFFFF;'>
+                        </td>
+                        <td style='background-color:#999999; color:#FFFFFF;'>".
+                            Yii::app()->format->format_decimal($Total->cost).
+                       "</td>
+                       <td style='background-color:#999999; color:#FFFFFF;'>".
+                            Yii::app()->format->format_decimal($Total->revenue).
                        "</td>
                     </tr>";
         }
