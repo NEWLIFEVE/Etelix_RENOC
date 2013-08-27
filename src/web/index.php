b18c23435a928c7e7da43538788015376d378cf9
NEWLIFEVE <mark182182@gmail.com>
2013-08-27 14:12:42 -0430

    commit por merge

--- src/web/index.php
+++ src/web/index.php
@@ -6,12 +6,10 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 //defined('YII_DEBUG') or define('YII_DEBUG',true);
-<<<<<<< HEAD
-// specify how many levels of call stack should be shown in each ldefined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-=======
+
 // specify how many levels of call stack should be shown in each log message
 //defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
->>>>>>> dev
+
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
