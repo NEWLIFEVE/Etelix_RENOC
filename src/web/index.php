5d636ac61dda7c015a52f3e02f404eca88f2e6d1
Manuel Zambrano <mmzmm3z@gmail.com>
2013-08-30 14:49:42 -0430

    preparando subida a serdev

--- src/web/index.php
+++ src/web/index.php
@@ -1,34 +1,4 @@
 <?php
-//
-//     function format_decimal($num,$decimales=3)
-//    {        
-//        $english_format_number2 = number_format($num, 10, ',', '.');
-//        $numtext=strval($english_format_number2);
-//        $position = strpos($numtext, ',');
-//        $numsub = substr($numtext,0,$position+$decimales); 
-//        return $numsub;
-//    }
-//    
-//$num=0.0000055;
-//$str1=  format_decimal($num);
-//if (strcmp($str1, "0,00")==0){
-//
-//    echo 'SI';
-//    echo strcmp($str1, "0");
-//    echo $str1;
-//}else{
-//    echo 'NO ';
-//    echo strcmp($str1, "0");
-//    echo $str1;
-//}
-//
-//if ($str1=="0,00"){
-//    echo 'tambien';
-//}else{
-//    echo 'tampoco';
-//}
-
-
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -64,12 +64,11 @@ return array(
         'db'=>array(
 
             //'connectionString'=>'pgsql:host=192.168.1.239;port=5432;dbname=sori',
-            //'connectionString'=>'pgsql:host=67.215.160.89;port=5432;dbname=sori',
             'connectionString'=>'pgsql:host=localhost;port=5432;dbname=sori',
 			'emulatePrepare'=>true,
 			'username'=>'postgres',
-			'password'=>'123',
-			//'password'=>'Nsusfd8263',
+			//'password'=>'123',
+			'password'=>'Nsusfd8263',
 			'charset'=>'utf8',
             ),
         'errorHandler'=>array(
