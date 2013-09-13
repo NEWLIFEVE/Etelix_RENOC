fda24f52326a6ee56a14a79becd216d95ed8900f
manuel Zambrano <mmzmm3z@gmail.com>
2013-09-13 15:34:35 -0430

    cambio de orden de checks para los reportes

--- src/web/protected/views/site/rutinarios.php
+++ src/web/protected/views/site/rutinarios.php
@@ -4,232 +4,144 @@
 */
 $this->layout=$this->getLayoutFile('menuContent');
 ?>
-    <script src="http://malsup.github.io/jquery.blockUI.js"></script>
-<div class="">
-    <div id="atras">
-        <ul class="thumbnails">
-            <li class="span1">
-                <div id="icono">
-                    <h1>
-                        <a id="flecha-backward" href="/" class="tooltip-test"><</a>
-                    </h1>
+<div class="row-fluid show-grid">
+    <div id="atras" class="span12">
+        <div class="span1 offset1">
+            <h1>
+                <a id="flecha-backward" href="/" class="tooltip-test"><</a>
+            </h1>
+        </div>
+        <div class="contiene">
+            <div class="titulosec">
+                RUTINARIOS
+            </div>
+            
+                <div class="calendario">
+                    <p>Seleccione una fecha</p>
+                    <div id="datepicker" class="span4">
+                    </div>
+                    <div id="mail" class="span1">
+                        <a href="/" rel="tooltip" title="Enviar Reportes a su Correo Electronico" class="tooltip-test">
+                            <img src="/images/mail.png" width="95px" height="95px" value="Activar Función">
+                        </a>
+                    </div>
+                    <div id="mailRenoc" class="span1">
+                        <a href="/" rel="tooltip" title="Enviar Reportes a Correo Electronico RENOC" class="tooltip-test">
+                            <img src="/images/mailRenoc.png" width="95px" height="95px" value="Activar Función">
+                        </a>
+                    </div>
+                    <div id="excel" class="span1">
+                        <a href="/" rel="tooltip" title="Exportar Reportes en Excel" class="tooltip-test">
+                            <img src="/images/excel.png" width="75px" height="75px" value="Activar Función">
+                        </a>
+                    </div>
                 </div>
-            </li>
-        </ul>
-    </div>
-    <div class="Rotate-90 hidden-phone hidden-tablet">
-        RUTINARIOS
-    </div>
-    <div id="barraVerde1" class="span2 hidden-phone">
-    </div>
-    <div id="instruccion" class="span1">
-        Seleccione una fecha
-    </div>
-    <div id="datepicker" class="span6">
-    </div>
-    <div id="mail" class="span1">
-        <a href="/" rel="tooltip" title="Enviar Reportes a su Correo Electronico" class="tooltip-test">
-            <img src="/images/mail.png" width="95px" height="95px" value="Activar Función">
-        </a>
-    </div>
-    <div id="mailRenoc" class="span1">
-        <a href="/" rel="tooltip" title="Enviar Reportes a Correo Electronico RENOC" class="tooltip-test">
-            <img src="/images/mailRenoc.png" width="95px" height="95px" value="Activar Función">
-        </a>
-    </div>
-    <div id="excel" class="span1">
-        <a href="/" rel="tooltip" title="Exportar Reportes en Excel" class="tooltip-test">
-            <img src="/images/excel.png" width="75px" height="75px" value="Activar Función">
-        </a>
-    </div>
-    <div id="barraVerde2" class="span2">
-    </div>
-    <div id="instruccion2" class="span1">
-        Seleccione los Reportes
-    </div>
-    <div id="tablagris" class="span2">
-        <form id="formRutinarios">
-        <table>
-            <tr>
-                <td width="47">
-                    <h3>
-                        <label class="checkbox">
-                            <input type="checkbox" value="true" id="todos" class="custom-checkbox" name="lista[todos]" onClick="marcar(this);">
-                            <label for="todos">
-                                <h4>
-                                </h4>
-                            </label>
-                        </label>
-                    </h3>
-                </td>
-                <td width="209" id="tdtodos">
-                    <label for="todos">
-                        <h4>
-                            Todos
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
-                        <input type="checkbox" value="true" id="AIR" class="custom-checkbox" name="lista[AIR]">
-                        <label for="AIR">
-                            <h4>
+                <form id="formRutinarios">
+                <div class="opciones">
+                    <p>Seleccione los Reportes</p>
+                    <div class="choice primeras">
+                        <input type="checkbox" value="true" id="todos" class="custom-checkbox" name="lista[todos]" onClick="marcar(this);">
+                        <label for="todos">
+                            <h4 id="td1">
+                                Todos
                             </h4>
                         </label>
-                    </label>
-                </td>
-                <td id="td1">
-                    <label for="AIR">
-                        <h4>
-                            Alto Impacto Retail(+1$)
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
+                    </div>
+                    <div class="choice primeras">
+                        <input type="checkbox" value="true" id="compraventa" class="custom-checkbox" name="lista[compraventa]">
+                        <label for="compraventa">
+                            <h4 id="td1">
+                                Ranking Compra Venta
+                            </h4>
+                        </label>
+                    </div>
+                    <div class="choice">
+                        <input type="checkbox" value="true" id="perdidas" class="custom-checkbox" name="lista[perdidas]">
+                        <label for="perdidas">
+                            <h4 id="td2">
+                                Perdidas
+                            </h4>
+                        </label>
+                    </div>
+                    <div class="choice">
                         <input type="checkbox" value="true" id="AI10" class="custom-checkbox" name="lista[AI10]">
                         <label for="AI10">
-                            <h4>
+                            <h4 id="td2">
+                                Alto Impacto(+10$)
                             </h4>
                         </label>
-                    </label>
-                </td>
-                <td id="td2">
-                    <label for="AI10">
-                        <h4>
-                            Alto Impacto(+10$)
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
+                    </div>
+                    <div class="choice">
                         <input type="checkbox" value="true" id="AI10V" class="custom-checkbox" name="lista[AI10V]">
                         <label for="AI10V">
-                            <h4>
+                            <h4 id="td3">
+                                Alto Impacto(+10$) por Vendedor
+                            </h4>
+                    </label>
+                    </div>
+                    <div class="choice">
+                        <input type="checkbox" value="true" id="AIR" class="custom-checkbox" name="lista[AIR]">
+                        <label for="AIR">
+                            <h4 id="td3">
+                                Alto Impacto Retail(+1$)
                             </h4>
                         </label>
-                    </label>
-                </td>
-                <td id="td2">
-                    <label for="AI10V">
-                        <h4>
-                            Alto Impacto(+10$) por Vendedor
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
+                    </div>
+                    <div class="choice">
                         <input type="checkbox" value="true" id="PN" class="custom-checkbox" name="lista[PN]">
                         <label for="PN">
-                            <h4>
+                            <h4 id="td4">
+                                Posicion Neta
                             </h4>
                         </label>
-                    </label>
-                </td>
-                <td id="td3">
-                    <label for="PN">
-                        <h4>
-                            Posicion Neta
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
+                    </div>
+                    <div class="choice">
                         <input type="checkbox" value="true" id="PNV" class="custom-checkbox" name="lista[PNV]">
                         <label for="PNV">
-                            <h4>
+                            <h4 id="td4">
+                                Posicion Neta por Vendedor
                             </h4>
                         </label>
-                    </label>
-                </td>
-                <td id="td3">
-                    <label for="PNV">
-                        <h4>
-                            Posicion Neta por Vendedor
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
-                        <input type="checkbox" value="true" id="DC" class="custom-checkbox" name="lista[DC]"> 
+                    </div>
+                    <div class="choice">
+                        <input type="checkbox" value="true" id="ATI" class="custom-checkbox" name="lista[ATI]">
+                        <label for="ATI">
+                            <h4 id="td5">
+                                Arbol de Trafico Internal
+                            </h4>
+                        </label>
+                    </div>
+                    <div class="choice">
+                        <input type="checkbox" value="true" id="ATE" class="custom-checkbox" name="lista[ATE]">
+                        <label for="ATE">
+                            <h4 id="td5">
+                                Arbol de Trafico External
+                            </h4>
+                        </label>
+                    </div>
+                    <div class="choice">
+                        <input type="checkbox" value="true" id="DC" class="custom-checkbox" name="lista[DC]">
                         <label for="DC">
-                            <h4>
+                            <h4 id="td5">
+                                Distribucion Comercial
                             </h4>
                         </label>
-                    </label>
-                </td>
-                <td id="td4">
-                    <label for="DC">
-                        <h4>
-                            Distribucion Comercial
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
-                        <input type="checkbox" value="true" id="perdidas" class="custom-checkbox" name="lista[perdidas]"> 
-                        <label for="perdidas">
-                            <h4>
-                            </h4>
-                        </label>
-                    </label>
-                </td>
-                <td id="td5">
-                    <label for="perdidas">
-                        <h4>
-                            Perdidas
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label class="checkbox">
-                        <input type="checkbox" value="true" id="compraventa" class="custom-checkbox" name="lista[compraventa]"> 
-                        <label for="compraventa">
-                            <h4>
-                            </h4>
-                        </label>
-                    </label>
-                </td>
-                <td id="td5">
-                    <label for="compraventa">
-                        <h4>
-                            RENOC Ranking Compra/Venta
-                        </h4>
-                    </label>
-                </td>
-            </tr>
-            <tr> 
-                <td>
-                </td> 
-                <td>
-                    <input name="fecha" type="hidden"  id="datepicker_value" value=" <?php
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <input name="fecha" type="hidden"  id="datepicker_value" value=" <?php
                     mktime(0, 0, 0, date("m"), date("d") + 1, date("Y"));
                     echo date("Y-m-d");
                     ?>"/>
-
-                </td>
-            </tr>
-        </table>
-        </form>
-    </div> 
-</div>
-<div id="respuesta">
-</div>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>            
 <script src="/js/jquery-ui.js"></script>
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/views.js"/></script>
+<script src="http://malsup.github.io/jquery.blockUI.js"></script>
+
