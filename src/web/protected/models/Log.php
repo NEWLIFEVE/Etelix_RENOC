68fc51dbc54a2f2ee3c8240058c6f9ee49817b67
manuel Zambrano <mmzmm3z@gmail.com>
2013-09-23 10:24:08 -0430

    arreglando errores

--- src/web/protected/models/Log.php
+++ src/web/protected/models/Log.php
@@ -111,6 +111,7 @@ class Log extends CActiveRecord
      * Funcion que devuelve true si la accion ya fue realizada en la fecha consultada
      * @param $id int numero de la accion
      * @param $fecha date fecha consultada
+     * @return boolean
      */
     public static function existe($id,$fecha)
     {
