6d85315e4c23492b563c31183f88d186a03e7aaf
manuel Zambrano <mmzmm3z@gmail.com>
2013-09-26 18:41:25 -0430

    responsive en vista index se site

--- src/web/protected/views/site/index.php
+++ src/web/protected/views/site/index.php
@@ -3,6 +3,29 @@
 $this->pageTitle = Yii::app()->name;
 ?>
 <div class="row">
+    <div id="capa">
+        <div id="barrablanca">
+        </div>
+        <div class="reportes">
+            <h1>RUTINARIOS
+                <a id="flecha-forward" href="/site/rutinarios"  rel="tooltip" title="esta es la consulta basica por hora y fecha" class="tooltip-test ">></a>
+            </h1>
+        </div>
+        <div class="reportes">
+            <h1> ESPECIFICOS
+                <a id="flecha-forward" href="/site/especificos" rel="tooltip" title="aqui se muestra informacion de data por fecha y hora especifica" class="tooltip-test">></a>
+            </h1>
+        </div>
+        <div class="reportes">
+            <h1> PERSONALIZADOS
+                <a id="flecha-forward" href="/site/personalizados" rel="tooltip" title="puede realizar una busqueda filtrada de data, por fecha, operadora, entre otras" class="tooltip-test">></a>
+            </h1>
+        </div>
+    </div>
+    <div class="div row-fluid">
+    </div>
+</div>
+<!--<div class="row">
         <div id="capa">
             <div id="barrablanca">
             </div>
@@ -43,4 +66,4 @@ $this->pageTitle = Yii::app()->name;
         <div class="div row-fluid">
         </div>
     <!--</div>-->
-</div>
+<!--</div>-->
