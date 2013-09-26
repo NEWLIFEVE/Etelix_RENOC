34db02936bc16920eb9573dd1c1083ebaac1b8e9
manuel Zambrano <mmzmm3z@gmail.com>
2013-09-26 15:38:23 -0430

    diseño adaptable en el login

--- src/web/protected/views/layouts/main.php
+++ src/web/protected/views/layouts/main.php
@@ -21,55 +21,55 @@
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 <body>
-    <div class="container" style="padding:-30px;">
-        <!--menu-->
-        <?php
-            $admin = (isset(Yii::app()->user->perfil) and Yii::app()->user->perfil == 'ADMIN') ? true : false ;
-            $this->widget('bootstrap.widgets.TbNavbar',array(
-                'type'=>'inverse',
-                'brand'=>'RENOC',
-                'brandUrl'=>'#',
-                'collapse'=>true,
-                'items'=>array(
-                    array(
-                        'class'=>'bootstrap.widgets.TbMenu',
-                        'htmlOptions'=>array(
-                            'class'=>'pull-right'
+    <!--menu-->
+    <?php
+        $admin = (isset(Yii::app()->user->perfil) and Yii::app()->user->perfil == 'ADMIN') ? true : false ;
+        $this->widget('bootstrap.widgets.TbNavbar',array(
+            'type'=>'inverse',
+            'brand'=>'RENOC',
+            'brandUrl'=>'#',
+            'collapse'=>true,
+            'items'=>array(
+                array(
+                    'class'=>'bootstrap.widgets.TbMenu',
+                    'htmlOptions'=>array(
+                        'class'=>'pull-right'
+                        ),
+                    'items'=>array(
+                        array(
+                            'label'=>'Usuarios',
+                            'url'=>array('/usersRenoc/admin'),
+                            'visible'=>$admin
                             ),
-                        'items'=>array(
-                            array(
-                                'label'=>'Usuarios',
-                                'url'=>array('/usersRenoc/admin'),
-                                'visible'=>$admin
-                                ),
-                            array(
-                                'label'=>'Login',
-                                'url'=>array('/site/login'),
-                                'visible'=>Yii::app()->user->isGuest
-                                ),
-                            array(
-                                'label'=>'Salir ('.Yii::app()->user->name.')',
-                                'url'=>array('/site/logout'),
-                                'visible'=>!Yii::app()->user->isGuest
-                                )
+                        array(
+                            'label'=>'Login',
+                            'url'=>array('/site/login'),
+                            'visible'=>Yii::app()->user->isGuest
                             ),
+                        array(
+                            'label'=>'Salir ('.Yii::app()->user->name.')',
+                            'url'=>array('/site/logout'),
+                            'visible'=>!Yii::app()->user->isGuest
+                            )
                         ),
                     ),
-                )
-            );
+                ),
+            )
+        );
+    ?>
+    <?php echo Yii::app()->bootstrap->init(); ?>
+    <div style="text-align: right;">
+        <?php
+            if(!Yii::app()->user->isGuest and isset(Yii::app()->user->last_login))
+            {
+                echo "Ultimo Acceso: ".Yii::app()->dateFormatter->format("d-M-y h:m a", Yii::app()->user->last_login);
+            }
         ?>
-        <div style="text-align: right;">
-            <?php
-                if(!Yii::app()->user->isGuest and isset(Yii::app()->user->last_login))
-                {
-                    echo "Ultimo Acceso: ".Yii::app()->dateFormatter->format("d-M-y h:m a", Yii::app()->user->last_login);
-                }
-            ?>
-        </div>
-        <!--fin de menu-->
-        <?php echo Yii::app()->bootstrap->init(); ?>
-        <?php echo $content; ?>
     </div>
+    <!--fin de menu-->
+    <!-- inicion de contenido-->
+    <?php echo $content; ?>
+    <!-- fin de contenido-->
     <div id="footer">
         Copyright &copy; <?php echo date('Y'); ?> SACET All Rights Reserved. Version 1.1.6
     </div>
