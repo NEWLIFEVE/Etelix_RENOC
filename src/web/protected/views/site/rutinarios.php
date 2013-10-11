a3311415ca7ce2804820f2323a4e5bb3c054cb5d
manuel Zambrano <mmzmm3z@gmail.com>
2013-10-11 15:21:10 -0430

    arregalndo un par de etiquetas sin cerrar

--- src/web/protected/views/site/rutinarios.php
+++ src/web/protected/views/site/rutinarios.php
@@ -45,145 +45,143 @@ $this->layout=$this->getLayoutFile('menuContent');
                     </label>
                 </div>
                 <div class="choice primeras">
-                        <input type="checkbox" value="true" id="compraventa" class="custom-checkbox" name="lista[compraventa]">
-                        <label for="compraventa">
-                            <h4 id="td1">
-                                Ranking Compra Venta
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="perdidas" class="custom-checkbox" name="lista[perdidas]">
-                        <label for="perdidas">
-                            <h4 id="td1">
-                                Perdidas
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="AIR" class="custom-checkbox" name="lista[AIR]">
-                        <label for="AIR">
-                            <h4 id="td1">
-                                Alto Impacto Retail(+1$)
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="AI10" class="custom-checkbox" name="lista[AI10]">
-                        <label for="AI10">
-                            <h4 id="td2">
-                                Alto Impacto(+10$)
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="AI10V" class="custom-checkbox" name="lista[AI10V]">
-                        <label for="AI10V">
-                            <h4 id="td2">
-                                AI (+10$) por Vendedor
-                            </h4>
+                    <input type="checkbox" value="true" id="compraventa" class="custom-checkbox" name="lista[compraventa]">
+                    <label for="compraventa">
+                        <h4 id="td1">
+                            Ranking Compra Venta
+                        </h4>
                     </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="PN" class="custom-checkbox" name="lista[PN]">
-                        <label for="PN">
-                            <h4 id="td2">
-                                Posicion Neta
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="PNV" class="custom-checkbox" name="lista[PNV]">
-                        <label for="PNV">
-                            <h4 id="td2">
-                                Posicion Neta por Vendedor
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="ADI" class="custom-checkbox" name="lista[ADI]">
-                        <label for="ADI">
-                            <h4 id="td3">
-                                Arbol Destinos Internal
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="ADE" class="custom-checkbox" name="lista[ADE]">
-                        <label for="ADE">
-                            <h4 id="td3">
-                                Arbol Destinos External
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="AC" class="custom-checkbox" name="lista[AC]">
-                        <label for="AC">
-                            <h4 id="td3">
-                                Arbol Clientes
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="AP" class="custom-checkbox" name="lista[AP]">
-                        <label for="AP">
-                            <h4 id="td3">
-                                Arbol Proveedores
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="DCV" class="custom-checkbox" name="lista[DCV]">
-                        <label for="DCV">
-                            <h4 id="td4">
-                                DC Vendedor
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="DCTP" class="custom-checkbox" name="lista[DCTP]">
-                        <label for="DCTP">
-                            <h4 id="td4">
-                                DC Termino Pago
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="DCM" class="custom-checkbox" name="lista[DCM]">
-                        <label for="DCM">
-                            <h4 id="td4">
-                                DC Monetizable
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="DCCom" class="custom-checkbox" name="lista[DCCom]">
-                        <label for="DCCom">
-                            <h4 id="td4">
-                                DC Compania
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="DCCarrier" class="custom-checkbox" name="lista[DCCarrier]">
-                        <label for="DCCarrier">
-                            <h4 id="td5">
-                                DC Carrier
-                            </h4>
-                        </label>
-                    </div>
-                    <div class="choice">
-                        <input type="checkbox" value="true" id="Ev" class="custom-checkbox" name="lista[Ev]">
-                        <label for="Ev">
-                            <h4 id="td5">
-                                Evolucion
-                            </h4>
-                        </label>
-                    </div>
-                    <input name="fecha" type="hidden"  id="datepicker_value" value="<?php $nuevafecha=strtotime('-1 day',strtotime(date('Y-m-d')));
-        echo $nuevafecha=date('Y-m-d',$nuevafecha);?>"/>
                 </div>
-
+                <div class="choice">
+                    <input type="checkbox" value="true" id="perdidas" class="custom-checkbox" name="lista[perdidas]">
+                    <label for="perdidas">
+                        <h4 id="td1">
+                            Perdidas
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="AIR" class="custom-checkbox" name="lista[AIR]">
+                    <label for="AIR">
+                        <h4 id="td1">
+                            Alto Impacto Retail(+1$)
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="AI10" class="custom-checkbox" name="lista[AI10]">
+                    <label for="AI10">
+                        <h4 id="td2">
+                            Alto Impacto(+10$)
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="AI10V" class="custom-checkbox" name="lista[AI10V]">
+                    <label for="AI10V">
+                        <h4 id="td2">
+                            AI (+10$) por Vendedor
+                        </h4>
+                </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="PN" class="custom-checkbox" name="lista[PN]">
+                    <label for="PN">
+                        <h4 id="td2">
+                            Posicion Neta
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="PNV" class="custom-checkbox" name="lista[PNV]">
+                    <label for="PNV">
+                        <h4 id="td2">
+                            Posicion Neta por Vendedor
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="ADI" class="custom-checkbox" name="lista[ADI]">
+                    <label for="ADI">
+                        <h4 id="td3">
+                            Arbol Destinos Internal
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="ADE" class="custom-checkbox" name="lista[ADE]">
+                    <label for="ADE">
+                        <h4 id="td3">
+                            Arbol Destinos External
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="AC" class="custom-checkbox" name="lista[AC]">
+                    <label for="AC">
+                        <h4 id="td3">
+                            Arbol Clientes
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="AP" class="custom-checkbox" name="lista[AP]">
+                    <label for="AP">
+                        <h4 id="td3">
+                            Arbol Proveedores
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="DCV" class="custom-checkbox" name="lista[DCV]">
+                    <label for="DCV">
+                        <h4 id="td4">
+                            DC Vendedor
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="DCTP" class="custom-checkbox" name="lista[DCTP]">
+                    <label for="DCTP">
+                        <h4 id="td4">
+                            DC Termino Pago
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="DCM" class="custom-checkbox" name="lista[DCM]">
+                    <label for="DCM">
+                        <h4 id="td4">
+                            DC Monetizable
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="DCCom" class="custom-checkbox" name="lista[DCCom]">
+                    <label for="DCCom">
+                        <h4 id="td4">
+                            DC Compania
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="DCCarrier" class="custom-checkbox" name="lista[DCCarrier]">
+                    <label for="DCCarrier">
+                        <h4 id="td5">
+                            DC Carrier
+                        </h4>
+                    </label>
+                </div>
+                <div class="choice">
+                    <input type="checkbox" value="true" id="Ev" class="custom-checkbox" name="lista[Ev]">
+                    <label for="Ev">
+                        <h4 id="td5">
+                            Evolucion
+                        </h4>
+                    </label>
+                </div>
+                <input name="fecha" type="hidden"  id="datepicker_value" value="<?php $nuevafecha=strtotime('-1 day',strtotime(date('Y-m-d')));
+    echo $nuevafecha=date('Y-m-d',$nuevafecha);?>"/>
             </form>
         </article>
     </section>
