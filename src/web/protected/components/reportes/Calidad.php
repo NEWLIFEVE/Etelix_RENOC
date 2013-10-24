ad2fc17e837feb9cc980ee1e6ac920e16d1ec9fa
manuel Zambrano <mmzmm3z@gmail.com>
2013-10-24 12:49:32 -0430

    agregado metodo getHtmlDestinations

--- src/web/protected/components/reportes/Calidad.php
+++ src/web/protected/components/reportes/Calidad.php
@@ -6,7 +6,51 @@
 class Calidad extends Reportes
 {
 	/**
-	 * 
+	 * Metodo encargado de generar la tabla de html con las fechas pasadas
+	 * @access public
+	 * @static
+	 * @param date $startDate fecha de inicio de la consulta
+	 * @param date $endingDate fecha fin de la consulta
+	 * @param int $carrier id del carrier que se quiere consultar
+	 * @return string $cuerpo html de la tabla construida
+	 */
+	public static function getHtmlDestinations($startDate,$endingDate,$carrier)
+	{
+		$model=self::getDestinations($startDate,$endingDate,$carrier);
+		if($model!=null)
+		{
+			$cuerpo="<table>";
+			$cuerpo.="<tr><td colspan='11'>".Carrier::getName($carrier)."</td></tr>";
+        	$cuerpo.=self::cabecera(array('Ranking','Destino','Minutos','Intentos Totales','Intentos Completados','Intentos NC Inc. RI','Intentos NC Exc. RI','ASR Inc. RI','ASR Exc. RI','&Delta; ASR','ACD'),'background-color:#615E5E; color:#62C25E; width:10%; height:100%;');
+        	foreach ($model as $key => $destino)
+        	{
+        		$cuerpo.="<tr>
+        					<td>".$key+1."</td>
+        					<td>".$destino->destino."</td>
+        					<td>".$destino->minutes."</td>
+        					<td>".$destino->total_calls."</td>
+        					<td>".$destino->complete_calls_exc."</td>
+        					<td>".$destino->incomplete_calls_inc."</td>
+        					<td>".$destino->incomplete_calls_exc."</td>
+        					<td>".$destino->asr_inc."</td>
+        					<td>".$destino->asr_exc."</td>
+        					<td>".$destino->delta."</td>
+        					<td>".$destino->acd."</td><td></td></tr>";
+        	}
+			$cuerpo.="</table>";
+			return $cuerpo;
+		}
+		return false;
+	}
+
+	/**
+	 * Metodo encargado de traer de base de datos los destinos de calidad de un proveedor pasado
+	 * @access private
+	 * @static
+	 * @param date $startDate fecha de inicio de consulta
+	 * @param date $endingDate fecha fin de la consulta
+	 * @param int $carrier id del carrier que quieres consultar
+	 * @return object $model
 	 */
 	private static function getDestinations($startDate,$endingDate,$carrier)
 	{
@@ -24,7 +68,7 @@ class Calidad extends Reportes
 			  	   	    GROUP BY id_destination) b
 				   GROUP BY id_destination
 				   ORDER BY minutes DESC) b, destination d
-			  WHERE b.id_destination=d.id";
+			  WHERE b.id_destination=d.id AND b.minutes>0";
 		return Balance::model()->findAllBySql($sql);
 	}
 }
