c20a2da8ea6454024df2ea6de6e6345aec34a7a1
manuel Zambrano <mmzmm3z@gmail.com>
2013-10-28 19:21:02 -0430

    agregada funcion de creeacion de elementos

--- src/web/js/renoc.js
+++ src/web/js/renoc.js
@@ -14,11 +14,11 @@
  	 */
  	function init()
  	{
- 		$(".fecha").on('click',function()
+ 		var checkFecha=document.getElementsByName('lista[fecha]');
+ 		if(checkFecha!="undefined")
  		{
- 			console.log("hey");
- 			//_changeClass($('.fecha label h4'),'stretchRight');
- 		});
+ 			checkFecha[0].onclick=function(){_changeClass($('.fecha label h4'),'stretchRight','offStretchRight')};
+ 		}
  	}
 
  	/**
@@ -26,16 +26,41 @@
 	 * @access private
 	 * @param jQuery obj es el objeto de la fila que se quiere manipular
 	 */
-	function _changeClass(obj,nameClass)
+	function _changeClass(obj,activeClass,desactiveClass)
 	{
-		if(obj.attr('class')==nameClass)
+		if(obj.attr('class')==activeClass)
 		{
-			obj.removeClass(nameClass);
+			obj.removeClass(activeClass).addClass(desactiveClass);
 		}
 		else
 		{
-			obj.addClass(nameClass);
+			obj.removeClass(desactiveClass).addClass(activeClass);
 		}
+		_showCheck();
+		obj=null;
+	}
+
+	/**
+	 *
+	 */
+	function _createElement(element,id,name)
+	{
+		newElement=document.createElement(element);
+		newElement.id=id;
+		newElement.name=name;
+		return newElement;
+	}
+
+	/**
+	 *
+	 */
+	function _showCheck()
+	{
+		start=$(_createElement('input','startDate','startDate')).datepicker().css('display','none');
+		ending=$(_createElement('input','endingDate','endingDate')).datepicker().css('display','none');
+		$("div.choice_parametros.fecha").append(start,ending);
+		start.fadeIn('slow');
+		ending.fadeIn('slow');
 	}
 
 	/**
