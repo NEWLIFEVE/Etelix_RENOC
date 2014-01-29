3abb762862e08f56f7a6515e15831bbb2179ab64
Manuel Zambrano <mmzmm3z@gmail.com>
2014-01-29 11:55:46 -0430

    cambio de numero

--- src/web/protected/views/layouts/main.php
+++ src/web/protected/views/layouts/main.php
@@ -76,7 +76,7 @@
     <?php echo $content; ?>
     <!-- fin de contenido-->
     <div id="footer">
-        Copyright &copy; <?php echo date('Y'); ?> SACET All Rights Reserved. Version 1.3.17
+        Copyright &copy; <?php echo date('Y'); ?> SACET All Rights Reserved. Version 1.3.18
     </div>
     <div class="clear"></div>
     <div class='cargandosori'>
