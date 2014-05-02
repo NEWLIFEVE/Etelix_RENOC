80a42f3e9b162ae03c758c6bc64bde0e05c63246
Manuel Zambrano <mmzmm3z@gmail.com>
2014-05-02 12:38:41 -0430

    otra ip de prueba

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -56,7 +56,7 @@ return array(
             'routes'=>array(
                 array(
                     'class'=>'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
-                    'ipFilters'=>array('127.0.0.1','192.168.1.215'),
+                    'ipFilters'=>array('127.0.0.1','67.215.160.89'),
                 ),
                 // uncomment the following to show log messages on web pages
                 /*
