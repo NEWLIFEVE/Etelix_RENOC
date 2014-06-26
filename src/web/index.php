41cf83d9f047f253092c5c7e8672b45344026745
Manuel Zambrano <mmzmm3z@gmail.com>
2014-06-26 11:59:04 -0430

    cambios de entornos

--- src/web/index.php
+++ src/web/index.php
@@ -1,24 +1,15 @@
 <?php
 //Definimos nuestro servidor de produccion 
 define('SERVER_NAME_PROD','renoc.sacet.com.ve');
-//Definimos nuestro servidor de preproduccion 
-define('SERVER_NAME_PRE_PROD','devr.sacet.com.ve');
-//Definimos nuestro servidor de desarrollo 
-define('SERVER_NAME_DEV','renoc.local');
 //Obtenemos el nombre del servidor actual 
 $server=$_SERVER['SERVER_NAME'];
 
 switch ($server)
 {
 	case SERVER_NAME_PROD:
-		defined('YII_DEBUG') or define('YII_DEBUG',true);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+		defined('YII_DEBUG') or define('YII_DEBUG',false);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
 		break;
-	case SERVER_NAME_PRE_PROD:
-		defined('YII_DEBUG') or define('YII_DEBUG',true);
-		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-		break;
-	case SERVER_NAME_DEV:
 	default:
 		defined('YII_DEBUG') or define('YII_DEBUG',true);
 		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
@@ -26,9 +17,11 @@ switch ($server)
 }
 $yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
 require_once($yii);
+
 $main=require(dirname(__FILE__).'/protected/config/main.php');
 $db=require(dirname(__FILE__).'/protected/config/db.php');
+$gii=require(dirname(__FILE__).'/protected/config/gii.php');
 
-$config=CMap::mergeArray($main,$db);
+$config=CMap::mergeArray($main,$db,$gii);
 
 Yii::createWebApplication($config)->run();
